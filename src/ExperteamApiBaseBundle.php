1a65c046063f6a01e12fb17b5d4eb91256ea5326
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-01-27 11:30:21 -0500

    refactor(bundle): removed doctrine compiler pass.

--- src/ExperteamApiBaseBundle.php
+++ src/ExperteamApiBaseBundle.php
@@ -4,15 +4,10 @@ namespace Experteam\ApiBaseBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 
 class ExperteamApiBaseBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(DoctrineOrmMappingsPass::createAnnotationMappingDriver(
-            ['Experteam\ApiBaseBundle\Entity'],
-            [realpath(__DIR__.'/Entity')]
-        ));
     }
 }
