626adcbb6113d6674448643a70b7e47507e44c94
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-04-19 14:56:08 -0500

    refactor: elk_logger get context without request and token.

--- src/Service/ELKLogger/ELKLogger.php
+++ src/Service/ELKLogger/ELKLogger.php
@@ -45,20 +45,25 @@ class ELKLogger implements ELKLoggerInterface
     {
         if (!isset($this->context)) {
             $cfgParams = $this->parameterBag->get('experteam_api_base.elk_logger');
-            $transactionId = $this->requestStack->getCurrentRequest()->get('transaction_id');
-
-            /** @var User $user */
-            $user = $this->tokenStorage->getToken()->getUser();
+            $request = $this->requestStack->getCurrentRequest();
+            $transactionId = !is_null($request) ? $request->get('transaction_id') : null;
 
             $this->context = [
                 'id' => empty($transactionId) ? uniqid() : $transactionId,
-                'user' => [
-                    'id' => $user->getId(),
-                    'username' => $user->getUsername()
-                ],
                 'channel' => $cfgParams['channel'],
                 'timestamp' => date_create()
             ];
+
+            $token = $this->tokenStorage->getToken();
+            if (!is_null($token)) {
+                /** @var User $user */
+                $user = $token->getUser();
+
+                $this->context['user'] = [
+                    'id' => $user->getId(),
+                    'username' => $user->getUsername()
+                ];
+            }
         }
 
         return $this->context;
