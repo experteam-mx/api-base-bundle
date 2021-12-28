50c96c7ad2e3b00b20e3ffc4107994ee771dc042
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-12-28 10:50:32 -0500

    feat: added http client service with jsend validation.

---
+++ src/Service/HttpClient/HttpClientInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Experteam\ApiBaseBundle\Service\HttpClient;
+
+use Closure;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+interface HttpClientInterface
+{
+    /**
+     * @param string $url
+     * @param mixed $body
+     * @param Closure|null $dataValidator
+     * @param array $query
+     * @param string|null $appKey
+     * @return array [status, message, response]
+     * @throws HttpException
+     */
+    public function post(string $url, $body, Closure $dataValidator = null, array $query = [], string $appKey = null): array;
+
+    /**
+     * @param string $url
+     * @param mixed $body
+     * @param Closure|null $dataValidator
+     * @param array $query
+     * @param string|null $appKey
+     * @return array [status, message, response]
+     * @throws HttpException
+     */
+    public function put(string $url, $body, Closure $dataValidator = null, array $query = [], string $appKey = null): array;
+
+    /**
+     * @param string $url
+     * @param array $query
+     * @param Closure|null $dataValidator
+     * @param string|null $appKey
+     * @return array [status, message, response]
+     * @throws HttpException
+     */
+    public function get(string $url, array $query = [], Closure $dataValidator = null, string $appKey = null): array;
+}
