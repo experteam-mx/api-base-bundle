fb9d2394cebf9d4aff5b0d6c43f1d26847c03198
Roberlay León Piñero <roberlayl@gmail.com>
2022-04-21 11:02:17 -0500

    fix(transaction): Adjusted service config

--- src/Service/Transaction/Transaction.php
+++ src/Service/Transaction/Transaction.php
@@ -2,18 +2,19 @@
 
 namespace Experteam\ApiBaseBundle\Service\Transaction;
 
-use Experteam\ApiRedisBundle\Service\RedisClient\RedisClientInterface;
+use Predis\Client;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Uid\Uuid;
 
 class Transaction implements TransactionInterface
 {
     /**
-     * @var RedisClientInterface
+     * @var Client
      */
-    private $redisClient;
+    private $predisClient;
 
     /**
      * @var ParameterBagInterface
@@ -26,20 +27,27 @@ class Transaction implements TransactionInterface
     private $request;
 
     /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
      * @var string
      */
     private $key;
 
     /**
-     * @param RedisClientInterface $redisClient
+     * @param Client $predisClient
      * @param ParameterBagInterface $parameterBag
      * @param RequestStack $requestStack
+     * @param SerializerInterface $serializer
      */
-    public function __construct(RedisClientInterface $redisClient, ParameterBagInterface $parameterBag, RequestStack $requestStack)
+    public function __construct(Client $predisClient, ParameterBagInterface $parameterBag, RequestStack $requestStack, SerializerInterface $serializer)
     {
-        $this->redisClient = $redisClient;
+        $this->predisClient = $predisClient;
         $this->parameterBag = $parameterBag;
         $this->request = $requestStack->getCurrentRequest();
+        $this->serializer = $serializer;
     }
 
     /**
@@ -75,8 +83,9 @@ class Transaction implements TransactionInterface
     {
         $redisKey = $this->getRedisKey();
         $now = date_create()->format('YmdHisv');
-        $this->redisClient->hset($redisKey, "{$now}_$field", $value);
-        $this->redisClient->expire($redisKey, $this->parameterBag->get('app.transaction.ttl.sec'));
+        $value = $this->serializer->serialize($value, 'json');
+        $this->predisClient->hset($redisKey, "{$now}_$field", $value);
+        $this->predisClient->expire($redisKey, $this->parameterBag->get('app.transaction.ttl.sec'));
     }
 
     /**
@@ -90,6 +99,6 @@ class Transaction implements TransactionInterface
 
         return array_map(function ($v) {
             return json_decode($v);
-        }, ($this->redisClient->hgetall($redisKey) ?? []));
+        }, ($this->predisClient->hgetall($redisKey) ?? []));
     }
 }
