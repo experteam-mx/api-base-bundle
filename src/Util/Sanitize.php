f736bf8772526dec37ca85049dcf950093e656b3
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2022-06-17 12:40:27 -0500

    feat: added sanitize util class

---
+++ src/Util/Sanitize.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Experteam\ApiBaseBundle\Util;
+
+class Sanitize
+{
+    const ALLOWS = [' ', '!', '@', '<', '>', '$', '#', '%', '&', '*', 'ñ', 'Ñ', '(', ')', '.', ','];
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function integer(string $value)
+    {
+        return filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function numeric(string $value)
+    {
+        return filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function email(string $value)
+    {
+        return filter_var($value, FILTER_SANITIZE_EMAIL);
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function string(string $value)
+    {
+        $allows = implode('', self::ALLOWS);
+        return preg_replace("/[^A-Za-z0-9$allows]/u", '', $value);
+    }
+}
